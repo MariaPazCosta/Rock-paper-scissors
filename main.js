4fd80591463695a65760f0406bf1121afe0f980b
MariaPazCosta <mpazcostamagna@gmail.com>
2024-08-02 23:30:31 -0300

    all logic done, this version uses console.log instead of alerts

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function generateChoice(){
+
+
+function generateComputerChoice(){
     let max = 4
     let min = 1
     let option = Math.floor(Math.random() * (max - min) + min)
@@ -6,20 +8,67 @@ function generateChoice(){
 }
 
 // 1 es piedra, 2 es papel y 3 es tijeras
-function showComputerChoice(choice){
+function showChoice(choice, player){
     if(choice==1){
-        alert("The computer choose rock")
+        console.log(player + " has chosen rock")
     }else if(choice==2){
-        alert("The computer choose paper")
+        console.log(player + " has chosen paper")
     }else{
-        alert("The computer choose scissors")
+        console.log(player +" has chosen scissors")
     }
 }
 
-function showPlayerChoice(){
+
+let playerName= prompt("Enter your name")
+
+
+
+function playGame(){
+    let humanScore = 0
+    let computerScore = 0
     
+    for(i=0; i< 5; i++){
+        let humanChoice= prompt("Choose between: 1: rock , 2: paper 3: scissors");
+        let compChoice= generateComputerChoice()
+        
+        showChoice(compChoice, "Computer")
+        showChoice(humanChoice, playerName)
+        playRound(humanChoice,compChoice)
+    }
+    
+    function playRound (humanChoice, compChoice){
+        if(humanChoice == 1 && compChoice == 3){
+            console.log("Human wins this round!")
+            humanScore+=10
+        }else if(humanChoice == 2 && compChoice == 1){
+            console.log("Human wins this round!")
+            humanScore+=10
+        }else if(humanChoice==3 && compChoice == 2){
+            console.log("Human wins this round!")
+            humanScore+=10
+        }else if (compChoice == humanChoice){
+            console.log("It is a tie!")
+            
+        }else{
+            console.log("Computer wins")
+            computerScore+=10
+        }
+            
+    }
+
+    if(humanScore > computerScore){
+        console.log(playerName+ "Is the winner")
+        console.log(playerName+ " score is: "+humanScore)
+        console.log("Computer score is: " + computerScore)
+    }else if (humanScore == computerScore){
+        console.log("It is a tie!")
+        console.log(playerName+ " score is: "+humanScore)
+        console.log("Computer score is: " + computerScore)
+    }else{
+        console.log( "The computer is the winner")
+        console.log(playerName+ " score is: "+humanScore)
+        console.log("Computer score is: " + computerScore)
+    }
 }
 
-
-let compChoice= generateChoice()
-showComputerChoice(compChoice)
+playGame()
